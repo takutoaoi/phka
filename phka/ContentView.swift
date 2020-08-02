be18133c66ea074ac9263455e8ee7973bc85cf03
aoi takuto <a.013053@gmail.com>
2020-08-02 14:55:53 +0900

    init

---
+++ phka/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  phka
+//
+//  Created by a on 2020/08/02.
+//  Copyright © 2020 a. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
